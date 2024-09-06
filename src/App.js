ccf2383e5ba9c02baa43d6b075f910c5b2d7b59e
s1f102302753 <s1f102302753@iniad.org>
2024-09-06 22:20:52 +0900

    レシピのリスト表示機能追加

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
+import RecipeList from "./RecipeList";
 
 function App() {
-  const [foodName, setFoodName] = useState([]);
-  const [recipe, setRecipe] = useState("");
+  const [foodName, setFoodName] = useState("");
+  const [date, setDate] = useState("");
+  const [recipes, setRecipes] = useState([]);
 
   const onChangeFoodName = (e) => {
     setFoodName(e.target.value);
   };
 
+  const onChangeDate = (e) => {
+    setDate(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("料理:", foodName);
-    setFoodName("");
+    if (foodName.trim() && date) {
+      setRecipes([...recipes, {foodName, date}]);
+      setFoodName("");
+      setDate("");
+    }
   };
 
   return (
@@ -19,7 +28,8 @@ function App() {
       <div className="main-area">
         <h1>レシピ記録</h1>
       </div>
-
+      
+      <hr />
       <div className="add-recipe">
         <h3>レシピ追加</h3>
         <form onSubmit={handleSubmit}>
@@ -28,8 +38,19 @@ function App() {
             onChange={onChangeFoodName}
             placeholder="料理名を登録"
           />
+          <small>日付</small>
+          <input
+            type="date"
+            value={date}
+            onChange={onChangeDate}
+          />
           <button type="submit">登録</button>
         </form>
+      </div>
+
+      <hr />
+      <div className="recipe-list">
+        <RecipeList recipes={ recipes } />
       </div>
     </>
   );
