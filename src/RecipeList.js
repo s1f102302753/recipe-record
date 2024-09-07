d43f13a397bb9de7f32c339c024a55fd9c320e01
s1f102302753 <s1f102302753@iniad.org>
2024-09-07 22:40:42 +0900

    記録機能に写真も載せられるようにしました

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -7,8 +7,15 @@ const recipeList = ({ recipes }) => {
             <ul>
                 {recipes.map((recipe, index) => (
                     <li key={index} className="recipe-item">
-                        <span className="recipe-name">{recipe.foodName} : </span>
-                        <span className="recipe-date">{recipe.date}</span>
+                        <span className="recipe-name">{recipe.foodName}</span>
+                        {recipe.image && (
+                            <img
+                                src={recipe.image}
+                                alt={recipe.foodName}
+                                style={{ maxWidth: "100px", maxHeight: "100px" }}
+                            />
+                        )}
+                        <p><small className="recipe-date">{recipe.date}</small></p>
                     </li>
                 ))}
             </ul>
