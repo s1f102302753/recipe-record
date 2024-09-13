3e9795a5d92363f911b5f679efa523c9e6ee500f
s1f102302753 <s1f102302753@iniad.org>
2024-09-13 23:33:42 +0900

    コメント機能を追加

--- src/DetailScreen.js
+++ src/DetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, useNavigate } from "react-router-dom";
 
 const DetailScreen = ({ recipes }) => {
@@ -6,9 +6,28 @@ const DetailScreen = ({ recipes }) => {
     const navigate = useNavigate();
     const selectedRecipe = recipes.find((recipe) => recipe.id === id);
 
+    //ローカルストレージから初期文を取得
+    const [comment, setComment] = useState(selectedRecipe.comment || "");   //コメント入力
+    const storedComment = localStorage.getItem(`recipe-${id}-comment`);
+
+    //コメントが変更されたときにローカルストレージに保存する
+    const handleCommentChange = (e) => {
+        const newComment = e.target.value;
+        setComment(newComment);
+        localStorage.setItem(`recipe-${id}-comment`, newComment);   //ローカルストレージに保存
+    };
+
+    useEffect(() => {
+        const storedData = localStorage.getItem(`recipe-${id}-comment`);
+        if (storedData) {
+            setComment(storedData);
+        }
+    }, [id]);
+
+
     //レシピの情報を取得して表示する処理を追加
     return (
-        <div>
+        <div className='detail'>
             <h2>レシピの詳細</h2>
             <p>{selectedRecipe.date}</p>
             {selectedRecipe ? (
@@ -25,6 +44,17 @@ const DetailScreen = ({ recipes }) => {
             ) : (
                 <p>レシピが見つかりません。</p>
             )}
+
+            
+
+            <textarea
+                value={comment}
+                onChange={handleCommentChange}
+                placeholder="コメント"
+                style={{ width: "100%", height: "100px"}}
+            />
+
+            <br></br>
             <button onClick={() => navigate(-1)}>
                 戻る
             </button>
